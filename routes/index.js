1a28a7233f366f8860afacc10d60ff2471935ffe
Cosimo Russo <cosimo@menuale.com>
2019-05-06 23:54:04 +0200

    Add asyncMiddleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let router = express.Router();
+const asyncMiddleware = require("../middlewares/asyncMiddleware");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncMiddleware((req, res, next) => {
   res.render('index', { title: 'Express' });
-});
+}));
 
 module.exports = router;
