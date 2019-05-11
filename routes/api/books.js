ae089878d9f36b737141810621bed67d222ccde6
Cosimo Russo <cosimo@menuale.com>
2019-05-11 14:14:01 +0200

    Add genre and theme to book

--- routes/api/books.js
+++ routes/api/books.js
@@ -3,6 +3,8 @@ let router = express.Router();
 const asyncMiddleware = require("../../middlewares/asyncMiddleware");
 const Book = require("../../models").book;
 const Author = require("../../models").author;
+const Theme = require("../../models").theme;
+const Genre = require("../../models").genre;
 const Op = require("../../models").Sequelize.Op;
 
 router.get("/", asyncMiddleware( async (req, res, next) => {
@@ -17,6 +19,21 @@ router.post("/", asyncMiddleware( async (req, res, next) => {
   if(!authors.length===req.body.authors.length) return next("Author not found");
   const newBook = await Book.create(req.body);
   await newBook.setAuthors(authors);
+  const {genres, themes} = req.body;
+  if(genres){
+    genres.forEach((name) => {
+      Genre.findOrCreate({where: {name}}).then(g => {
+        newBook.addGenre(g[0]).catch(console.error);
+      });
+    });
+  }
+  if(themes){
+    themes.forEach((name) => {
+      Theme.findOrCreate({where: {name}}).then(t => {
+        newBook.addTheme(t[0]).catch(console.error);
+      });
+    });
+  }
   res.json(newBook);
 }));
 
