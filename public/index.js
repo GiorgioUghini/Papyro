98f6f2cc2a7ea3ec9751c1cc4673ad2f50932e9a
GiorgioUghini <giorgio1937@hotmail.it>
2019-05-14 14:31:53 +0200

    Add card data fetching

---
+++ public/index.js
@@ -0,0 +1,18 @@
+$.ajax({
+    url:"/api/books",
+    type: "GET",
+    success:function(result){
+        let $card = $("#card");
+        let $cardsContainer = $("#cards-container");
+        result.forEach(card => {
+            let c = $card.html().replace("%title%", card.title)
+                .replace("%abstract%", card.abstract)
+                .replace("%theme%", card.themes.join(" "))
+                .replace("%picture%", card.picture);
+            $cardsContainer.append(c);
+        });
+    },
+    error: function(richiesta,stato,errori){
+        alert(1);
+    }
+});
