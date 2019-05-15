6ce00f5389c1151c332319c2f6c33ee9ea47ccd3
Cosimo Russo <cosimo@menuale.com>
2019-05-15 15:04:39 +0200

    Implement reservation api

--- routes/api/reservations.js
+++ routes/api/reservations.js
@@ -4,6 +4,7 @@ const asyncMiddleware = require("../../middlewares/asyncMiddleware");
 const authMiddleware = require("../../middlewares/requiresAuth");
 const Reserve = require("../../models").reserve;
 const Cart = require("../../models").cart;
+const sequelize = require("../../models").Sequelize;
 const {mapToArray} = require("../../utils");
 const createError = require("http-errors");
 
@@ -38,6 +39,24 @@ router.post("/removeFromCart", authMiddleware, asyncMiddleware(async (req, res, 
   const cartElement = await Cart.findOne({where: {bookId, userId}});
   if(!cartElement) throw createError(409, "Book was not in the cart");
   await cartElement.destroy();
+  res.json({msg: "ok"});
+}));
+
+router.post("/confirmReservation", authMiddleware, asyncMiddleware(async (req, res, next) => {
+  const userId = req.user.id;
+  const cart = await Cart.findAll({where: {userId}});
+  let maxReservationId = await Reserve.findAll({
+      attributes: [[sequelize.fn('MAX', sequelize.col('reservationId')), 'maxReservationId']]
+    });
+  maxReservationId = maxReservationId[0].getDataValue("maxReservationId") || 0;
+  for(let cartElement of cart){
+    await Reserve.create({
+      bookId: cartElement.bookId,
+      userId,
+      reservationId: maxReservationId+1
+    });
+    await cartElement.destroy();
+  }
   res.json({msg: "ok"});
 }));
 
