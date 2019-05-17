608b3cb743a9bf027de9be2d02ad831eec8b1cef
GiorgioUghini <giorgio1937@hotmail.it>
2019-05-17 11:36:43 +0200

    Finish home and ourbooks

--- public/assets/js/ourbooks.js
+++ public/assets/js/ourbooks.js
@@ -6,8 +6,9 @@ $.ajax({
         let $cardsContainer = $("#cards-container");
         result.forEach(card => {
             let c = $card.html().replace("%title%", card.title)
-                .replace("%abstract%", card.abstract)
-                .replace("%theme%", card.themes.join(" "))
+                .replace("%abstract%", card.abstract.truncate())
+                .replace("%maintheme%", card.themes[0])
+                .replace("%maingenre%", card.genres[0])
                 .replace("%picture%", card.picture);
             $cardsContainer.append(c);
         });
@@ -16,3 +17,12 @@ $.ajax({
         alert("Error");
     }
 });
+
+String.prototype.truncate = function(){
+    var re = this.match(/^.{0,315}[\S]*/);
+    var l = re[0].length;
+    var re = re[0].replace(/\s$/,'');
+    if(l < this.length)
+        re = re + "&hellip;";
+    return re;
+}
