2f1ee22e5f8ad79ee1e18744b101e468b8ddd0f3
Cosimo Russo <cosimo@menuale.com>
2019-05-22 21:59:29 +0200

    Add basic get for single author

--- routes/api/authors.js
+++ routes/api/authors.js
@@ -2,13 +2,24 @@ let express = require('express');
 let router = express.Router();
 const asyncMiddleware = require("../../middlewares/asyncMiddleware");
 const Author = require("../../models").author;
+const createError = require("http-errors");
 
-router.get("/", asyncMiddleware( async (req, res, next) => {
+router.get("/", asyncMiddleware( async (req, res) => {
   const authors = await Author.findAll();
   res.json(authors);
 }));
 
-router.post("/", asyncMiddleware( async (req, res, next) => {
+router.get("/:id", asyncMiddleware(async (req, res) => {
+  const author = await Author.findOne({
+    where: {
+      id: req.params.id
+    }
+  });
+  if(!author) throw createError(404);
+  res.json(author);
+}));
+
+router.post("/", asyncMiddleware( async (req, res) => {
   const newAuthor = await Author.create(req.body);
   res.json(newAuthor);
 }));
