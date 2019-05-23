606c76137ebdb569da6d67288250f7fc63f912be
GiorgioUghini <giorgio1937@hotmail.it>
2019-05-23 09:33:42 +0200

    Hide login-hide and login-show elements by default, then use j.query.show() only

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,22 +1,16 @@
 $(document).ready(function () {
   const token = Cookies.get("token");
-  if(token){
+  if(token) {
     $(".login-show").each(function () {
       $(this).show();
     });
-    $(".login-hide").each(function () {
-      $(this).hide();
-    });
-  }else{
-    $(".login-show").each(function () {
-      $(this).hide();
-    });
+  } else {
     $(".login-hide").each(function () {
       $(this).show();
     });
   }
 
-  $("#signoutButton").click(function(e){
+  $("#signoutButton").click(function(e) {
     e.preventDefault();
     Cookies.remove("token");
     window.location.href = "/";
