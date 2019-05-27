6f27d7b09c35d9d7a68743ab2384e5c1e1d66424
Cosimo Russo <cosimo@menuale.com>
2019-05-27 15:09:31 +0200

    Create cart page

---
+++ public/assets/js/cart.js
@@ -0,0 +1,46 @@
+$(document).ready(async function () {
+  $("#emptyCart").hide();
+  const token = Cookies.get("token");
+  if(!token) window.location.href = "/login";
+  let cart = await fetch("/api/reservations/cart", {
+    method: "get",
+    headers: new Headers({
+      authorization: token
+    })
+  });
+  cart = await cart.json();
+  if(!cart.length) showEmptyCart();
+  const cartTable = $("#cartTable");
+  const cartRow = $("#cartRow").html();
+  for (let bookId of cart){
+    let book = await fetch("/api/books/"+bookId);
+    book = await book.json();
+    const newRow = cartRow
+      .replace("%title%", book.title)
+      .replace(/%bookId%/g, book.id);
+    cartTable.append(newRow);
+  }
+
+  $(".removeButton").click(async function () {
+    const bookId = $(this).data("book-id");
+    const result = await fetch("/api/reservations/removeFromCart", {
+      method: "post",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        authorization: token
+      }),
+      body: JSON.stringify({
+        bookId
+      })
+    });
+    if(result.ok){
+      $(`.cartRow[data-book-id="${bookId}"]`).remove();
+      if($(".removeButton").length === 1) showEmptyCart();
+    }
+  });
+});
+
+function showEmptyCart(){
+  $("#reserveButton").remove();
+  $("#emptyCart").show();
+}
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const asyncMiddleware = require("../middlewares/asyncMiddleware");
 
 /* GET home page. */
 router.get('/', asyncMiddleware( async (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Papyro' });
+}));
+
+router.get("/cart", asyncMiddleware(async (req, res, next) => {
+  res.render("cart", {title: "Papyro"});
 }));
 
 module.exports = router;
